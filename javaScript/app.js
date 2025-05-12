99c89ae69ccf402d64fb2396d802e8fb7f9c791c
hatimakhter <hatimakhter5@gmail.com>
2025-05-12 11:34:12 +0530

    update

---
+++ javaScript/app.js
@@ -0,0 +1,12 @@
+let color = "green";
+
+// traffic light system
+if(color === "red"){
+console.log("Stop!, the traffic light is Red");
+}
+if(color === "yellow"){
+console.log("Go slow , the traffic light is Yellow");
+}
+if(color === "green"){
+console.log("Go, the traffic light is Green");
+}
