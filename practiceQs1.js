aa1a0f57daf67dfc89726ab65201be3de383c6cb
hatimakhter <hatimakhter5@gmail.com>
2025-05-12 20:00:04 +0530

    Update and rename javaScript/app.js to practiceQs1.js

---
+++ practiceQs1.js
@@ -0,0 +1,11 @@
+console.log("traffic system");
+let color = "green";
+if(color === "red"){
+console.log("Stop!");
+}
+if(color === "yellow"){
+console.log("Go slow");
+}
+if(color === "green"){
+console.log("Go, the traffic light is Green..");
+}
