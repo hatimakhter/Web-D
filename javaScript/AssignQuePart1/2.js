3f803e4f0cc35332186d90a844f0b902a1b6c452
Hatim Akhter <hatimakhter5@gmail.com>
2025-08-04 23:49:44 +0530

    Update 2.js

--- javaScript/AssignQuePart1/2.js
+++ javaScript/AssignQuePart1/2.js
@@ -2,4 +2,4 @@ let Name = prompt("Enter your Name: ");
 let age = prompt("Enter your age: ");
 // let detail = Name + " " + "is " + age + " years old";
 // alert(detail);
-alert(` ${Name} is ${age} year old. `);
+alert(` ${Name} is ${age} year old. `); 
