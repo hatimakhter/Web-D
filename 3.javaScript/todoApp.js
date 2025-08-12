962abc9ff0f429d37fc620bb3f86ea3e150a1ec2
Hatim Akhter <hatimakhter5@gmail.com>
2025-08-12 23:04:53 +0530

    todoApp.js

--- 3.javaScript/todoApp.js
+++ 3.javaScript/todoApp.js
@@ -3,28 +3,26 @@ let req = prompt("Please enter your request ");
 while(true){
     if(req == "quit"){
         console.log("quitting the app");
-        break;
+        break; 
     }
     if(req == "list"){
         console.log("--------------------");
         for(let i = 0; i < todo.length; i++){
             console.log(i, todo[i]);
-        }
+        } 
         console.log("--------------------");    
-    }
+    } 
     else if(req == "add"){
         let task = prompt("Please enter the task you want to add");
         todo.push(task);
         console.log("task added");
-    }
+    } 
     else if(req == "delete"){
         let idx = prompt("please enter task Index");
         todo.splice(idx, 1);
         console.log("task delete");
-    }
+    } 
     else{
-        console.log("wrong request");
+        req = prompt("please enter your Request");
     }
-
-    req = prompt("please enter your Request");
 } 
