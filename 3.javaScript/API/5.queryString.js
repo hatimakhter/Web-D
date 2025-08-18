f2b7429b2f7230bc7ab57eac59f2814c4a1bab8e
hatimakhter <hatimakhter5@gmail.com>
2025-08-18 08:34:10 +0530

    api

---
+++ 3.javaScript/API/5.queryString.js
@@ -0,0 +1,35 @@
+let url = "http://universities.hipolabs.com/search?name=";
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async () => {
+    let country = document.querySelector("input").value;
+    console.log(country);
+
+    let collArr = await getCollege(country);
+    show(collArr);
+});
+
+function show(collArr){
+    let list = document.querySelector(".list");
+    list.innerText = "";
+    for(col of collArr){
+        console.log(col.name);
+
+        let li = document.createElement("li");
+        li.innerText = col.name;
+        list.appendChild(li);
+    }
+}
+
+let country = "nepal";
+
+async function getCollege(country) {
+    try{
+        let res = await axios.get(url + country);
+        return res.data;
+    }
+    catch(err){
+        console.log("error- ",err);
+        return [];
+    }
+}
